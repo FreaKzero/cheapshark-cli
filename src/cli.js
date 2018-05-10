08bc651c5346caa554192dbb691b9cb2d81925c7
FreaKzero <pete@freakzero.com>
2018-05-10 18:13:54 +0200

    Repair title sorting and listStores

--- src/cli.js
+++ src/cli.js
@@ -3,23 +3,21 @@ const meow = require('meow')
 const cli = meow(`
 Usage
   $ cheapshark <input>
+  --help            Show this usage dialog
+  --list-stores     List available stores
 
 Options
   --num,        -n  Number of Items to show                default: 30
   --sort        -s  [price,rating,title,deal,date]         default: date
   --order       -o  Order of items: [asc,desc]             default: asc
   --store       -b  Only view deals from a specific store  default: all
-  --list-stores -l  List available stores
-
+  
 Example
   $ cheapshark -n 3 -s title -o asc
   $ cheapshark --num 5 --store steam
 `, {
   listStores: {
-    store: {
-      type: 'string',
-      alias: 'l'
-    },
+    type: 'string'
   },
   flags: {
     store: {
--- src/getSort.js
+++ src/getSort.js
@@ -12,7 +12,7 @@ const getSort = (sort, orderStr) => {
     desc: {
       price: (a, b) => parseFloat(b.salePrice) - parseFloat(a.salePrice),
       rating: (a, b) => parseFloat(a.steamRatingPercent) < parseFloat(b.steamRatingPercent),
-      title: (a, b) => (a.internalName > b.internalName) ? -1 : (a.internalName < b.internalName) ? 1 : 0,
+      title: (a, b) => (b.internalName < a.internalName) ? -1 : (b.internalName > a.internalName) ? 1 : 0,
       deal: (a, b) => parseFloat(b.dealRating) - parseFloat(a.dealRating),
       date: (a, b) => parseInt(b.lastChange) - parseInt(a.lastChange)
     }
